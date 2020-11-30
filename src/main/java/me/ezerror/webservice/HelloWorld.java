07e02d84795bfa421b53c114fcfc123b9dd39020
ezerror <435719422@qq.com>
2020-11-30 23:35:40 +0800

    提交了ws相关

---
+++ src/main/java/me/ezerror/webservice/HelloWorld.java
@@ -0,0 +1,37 @@
+package me.ezerror.webservice;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * @author ：师源
+ * @date ：Created in 2020/11/12 9:46
+ * @description：
+ * @modified By：
+ * @version:
+ */
+
+
+@WebService
+public class HelloWorld {
+    @WebMethod
+    public String sayHello(String str) {
+        System.out.println("get Message...");
+        String result = "Hello World, " + str;
+        return result;
+    }
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        System.out.println("server is running");
+        String address = "http://localhost:9000/HelloWorld";
+        Object implementor = new HelloWorld();
+        Endpoint.publish(address, implementor);
+    }
+
+
+
+}
