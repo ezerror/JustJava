f8f6dcc9120cbd8bcc7d6b57493d2f1fdfceb633
ezerror <435719422@qq.com>
2021-01-10 22:21:00 +0800

    228

---
+++ src/main/java/notme/leetcode/_228.java
@@ -0,0 +1,31 @@
+package notme.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _228 {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ret = new ArrayList<String>();
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            int low = i;
+            i++;
+            while (i < n && nums[i] == nums[i - 1] + 1) {
+                i++;
+            }
+            int high = i - 1;
+            StringBuffer temp = new StringBuffer(Integer.toString(nums[low]));
+            if (low < high) {
+                temp.append("->");
+                temp.append(nums[high]);
+            }
+            ret.add(temp.toString());
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new _228().summaryRanges(new int[]{0, 1, 2, 4, 5, 7}));
+    }
+}
